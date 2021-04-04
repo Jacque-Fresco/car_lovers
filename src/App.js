70129a89a2872e30958c9d08e8186449556da461
Jacque-Fresco <haritonickd@gmail.com>
2021-04-04 16:50:57 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Fffff
+        </p>
+      </header>
+      <p>
+        rrrrsdrsa
+      </p>
+      <Button>
+        Hello
+      </Button>
+    </div>
+  );
+}
+
+export default App;
